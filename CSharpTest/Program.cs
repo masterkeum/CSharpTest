0452f48231fa2d994e55e1d17a9c6a499e6668db
masterkeum <masterkeum@gmail.com>
2023-12-29 14:00:11 +0900

    소스수정

--- CSharpTest/Program.cs
+++ CSharpTest/Program.cs
@@ -1,4 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("hello my name is kyonghee");
+
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("hello~!!!!");
+}
